fc7e61b912712d7f3e66f48e1ce6a5360c7b5ee8
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-17 23:27:13 -0300

    Feat: Adiciona cookie ao fastify.

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
 import fastify from 'fastify'
-// import { knex } from './database'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-// app.get('/hello', () => {
-//   return 'Hello World'
-// })
-
-// app.get('/hello', async () => {
-//   const transactions = await knex('transactions')
-//     .where('amount', 1000)
-//     .select('*')
-
-//   return transactions
-// })
-
-// app.register(transactionsRoutes)
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
