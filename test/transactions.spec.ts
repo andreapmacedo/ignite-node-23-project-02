7218166764d5945b4960da44588f3ce8330e16b2
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-18 18:06:39 -0300

    Feat: Adiciona configuração de banco de teste.

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,6 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+// import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,6 +13,11 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
   it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
@@ -21,6 +28,7 @@ describe('Transactions routes', () => {
       })
       .expect(201)
   })
+
   it('should be able to list all transactions', async () => {
     const createTransactionResponse = await request(app.server)
       .post('/transactions')
