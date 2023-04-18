ceaf2f80b410a09c00b75961862d86fd52a86c68
Andre Macedo <aa.andremacedo@gmail.com>
2023-04-18 13:48:15 -0300

    Feat: Adiciona teste de criação de transação.

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,6 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
+import { app } from './app'
 
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.addHook('preHandler', async (request, reply) => {
-  console.log(`[${request.method}] ${request.url}`)
-
-  // const sessionId = request.cookies.sessionId
-  // if (!sessionId) {
-  //   return reply.status(401).send({
-  //     error: 'Unauthorized.',
-  //   })
-  // }
-})
-
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
 
 app
   .listen({
